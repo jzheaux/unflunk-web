c2a7e241525cab4044e6618a3aa5742273d9f9b5
Josh Cummings <josh.cummings@gmail.com>
2014-09-23 07:39:49 -0600

    Changed signup messaging

--- src/signupView.php
+++ src/signupView.php
@@ -13,7 +13,7 @@
 				<h1 class="wordstripe centered"><span class="one">coming</span><span class="two">soon!</span></h1>
 				<input id="emailAddress" name="emailAddress" title="Email Addresss" value="Email Address" class="centered" size="40" name="q" style="color:#888" onfocus="inputFocus(this)" onblur="inputBlur(this)"/>
 				<div class="button" onclick="this.parentElement.submit()">Notify me</div>
-				<p id="comingsoonmessage" class="centered">Be notified when this service is available!</p>
+				<p id="comingsoonmessage" class="centered">Be notified when this service is available!  (We won't use your email address for anything else without your consent.)</p>
 			</form>
 		</div>
 <?php 		}
